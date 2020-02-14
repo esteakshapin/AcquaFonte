d9a318c06582d6cd50acfea3cc5bba7d3be1bc00
Esteak Shapin <esteakshapin@Esteaks-MBP.home>
2020-02-14 18:04:53 -0500

    got password and user name to be recieved by the server

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ssl
 app = Flask(__name__)
 app.secret_key = '6yTWFOE7j05WpVr8ic'
 
-client = MongoClient('mongodb://Shapin:Shapin@cluster0-shard-00-00-lnqyp.mongodb.net:27017,cluster0-shard-00-01-lnqyp.mongodb.net:27017,cluster0-shard-00-02-lnqyp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+client = MongoClient('mongodb://Shapin:Shapin@cluster0-shard-00-00-lnqyp.mongodb.net:27017,cluster0-shard-00-01-lnqyp.mongodb.net:27017,cluster0-shard-00-02-lnqyp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', ssl_cert_reqs=ssl.CERT_NONE)
 
 db = client['AqcuaFonte']
 users = db['users']
@@ -43,12 +43,34 @@ def log_in_page():
   elif request.method == 'POST':
     username = request.form['username']
     password = request.form['password']
-    return 'sucess'
+    if (users.count_documents({'username':username, 'password': password}) > 0):
+        print('found user')
+        return 'success'
+    else:
+        print("user not found",username,password)
+        return 'failure'
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    confirm_pass = request.form['confirm_pass']
+    if (confirm_pass == password):
+        if (users.count_documents({'username':username, 'password': password}) > 0):
+            alert('user already exists please log in')
+            return 'failure'
+        else:
+            return
+    else:
+        alert('passwords do not match up! Try Again')
+        return 'failure'
 
 
 
-  
+
+
+
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080, debug=True)
