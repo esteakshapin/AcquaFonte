2dbaaa3e0d80d477e7bea55b66d587f7766d2206
Esteak Shapin <esteakshapin@Esteaks-MBP.home>
2020-03-02 23:55:54 -0500

    got the home page to have the comparing image feature

--- static/index.js
+++ static/index.js
@@ -1,11 +1,14 @@
 $(function() {
-  $('.button2').hover(function() {
-    $('.image-new').css('width', '100%');
+  $('.button1').hover(function() {
+    $('.img-new').css('width', '100%');
     $('h1').css('color', 'black');
-
   }, function() {
     // on mouseout, reset the background colour
-    $('.image-new').css('width', '0%');
+    $('.img-new').css('width', '0%');
     $('h1').css('color', '#00adb5');
   });
 });
+$(document).ready(function () {
+  var a = $( '#original-img-id' ).css('width');
+  $('#new-img-id').css('width', a);
+})
