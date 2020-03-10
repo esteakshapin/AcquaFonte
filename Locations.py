4073c3602fe70ea57b3f19239dc2a3ac4eaa6716
Esteak Shapin <55769186+esteakshapin@users.noreply.github.com>
2020-03-09 23:36:59 -0400

    Delete Locations.py
    Useless file

--- Locations.py
+++
@@ -1,9 +0,0 @@
-class locations:
-  def __init__(self,name,lat,lon,sType, status, ratings, comments):
-    self.name = name
-    self.lat = lat
-    self.lon = lon
-    self.sType = sType
-    self.status = status
-    self.ratings = ratings
-    self.comments = comments
