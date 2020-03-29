7679199de6516cdf9fe20a0335ca3632ce7af620
Esteak Shapin <esteakshapin@gmail.com>
2020-03-28 23:45:43 -0400

    ready for deployment

--- main.py
+++ main.py
@@ -67,17 +67,17 @@ def get_fountains_in_range(lat, lon, distance_range):
   return list_return
 
 #adding header to disable caching -- REMOVE WHEN DEPLOYING SITE
-# @app.after_request
-# def add_header(r):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     r.headers['Cache-Control'] = 'public, max-age=0'
-#     return r
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 # Home Page
 @app.route('/', methods=['GET'])
@@ -343,7 +343,7 @@ def register():
 
 # def run():
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
 
 #def keep_alive():
 #    t = Thread(target=run)
