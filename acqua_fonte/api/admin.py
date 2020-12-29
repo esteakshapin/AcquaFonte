350cd4aae863185faf46d31ef3dd8e8eff3ea711
esteakshapin(mac) <esteakshapin@gmail.com>
2020-12-28 22:00:24 -0500

    removed outdated file

--- acqua_fonte/api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models.fountain import Fountain
-
-admin.site.register(Fountain)
--- acqua_fonte/api/models/update.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from .models import Fountain
-
-class Update(models.Model):
-    fountain_original = models.ForeignKey(Fountian, 
-                                          on_delete=models.CASCADE, 
-                                          related_name='fountian_updates')
-    
